1e03b5bdcac698ab0b3aea3267fd7ce78f2bc2c5
pangpange <eric0420@naver.com>
2024-12-01 15:07:04 +0900

    commit

--- src/main/java/com/nhnacademy/testtest/service/project/ProjectService.java
+++ src/main/java/com/nhnacademy/testtest/service/project/ProjectService.java
@@ -5,7 +5,11 @@ import com.nhnacademy.testtest.dto.project.ProjectPostRequest;
 import com.nhnacademy.testtest.entity.Project;
 import com.nhnacademy.testtest.entity.ProjectMember;
 
+import java.util.List;
+
 public interface ProjectService {
+
+    List<Project> getAllProjects();
 
     Project createProject(Project project, ProjectMember projectMember);
 
